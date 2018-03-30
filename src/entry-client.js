37ee8ba451e0e58346c0a508ae35edc9dbda8e8d
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-03-30 12:04:28 +0800

    fix permission identification mechanism bug on client-side

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,15 +1,56 @@
 import Vue from 'vue'
 import 'es6-promise/auto'
+import { ROLE_MAP, } from './constants'
 import { createApp, } from './app'
+import { filter, get, } from 'lodash'
+import { getProfile, } from './util/services'
 import ProgressBar from './components/ProgressBar.vue'
 
 // global progress bar
 const bar = Vue.prototype.$bar = new Vue(ProgressBar).$mount()
 document.body.appendChild(bar.$el)
 
+const debug = require('debug')('CLIENT:entry-client')
+let isInitialized = false
+
 // a global mixin that calls `asyncData` when a route component's params change
 Vue.mixin({
+  beforeRouteEnter (to, from, next) {
+    debug('router link enter somewhere.', to, from)
+    debug('isInitialized is true', isInitialized)
+    if (!isInitialized) {
+      isInitialized = true
+      next()
+    } else {
+      const permission = get(to, [ 'meta', 'permission', ])
+      if (permission) {
+        getProfile()
+        .then((profile) => {
+          const role = get(filter(ROLE_MAP, { key: get(profile, 'role'), }), [ 0, 'route', ], 'visitor')
+          if (permission !== 'member' && permission !== role) {
+            /** User doesn't have the right to go to route "to". So, go back to route "from" */
+            debug(`User doesn't have the right to go to route "to". So, go back to route "from"`)
+            next(from)
+          } else {
+            /** User approved to go to route "to". */
+            debug(`User approved to go to route "to".`)
+            next()
+          }
+        })
+        .catch(() => {
+          /** Cookie doesn't exist or fetching the profile in fail. So, go back to route "from". */
+          debug(`Cookie doesn't exist or fetching the profile in fail. So, go back to route "from".`)
+          next(from)
+        })
+      } else {
+        /** Route "to" doesn't have any permission setting. So, go to route "to" without problem. */
+        debug('Route "from" doesnt have any permission setting.')
+        next()
+      }
+    }
+  },
   beforeRouteUpdate (to, from, next) {
+    debug('router link to somewhere.', to.fullPath)
     const { asyncData, } = this.$options
     if (asyncData) {
       asyncData({
@@ -31,6 +72,7 @@ if (window.__INITIAL_STATE__) {
 }
 
 if (store.state.unauthorized) {
+  debug('entry-client resolved.')
   delete store.state.unauthorized
   router.push(store.state.targ_url)
 }
