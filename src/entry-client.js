06a0189bf9026708b8b9ee17314c0948bbfe8554
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-05-08 11:04:38 +0800

    fetch data when beforeRouteUpdate on profile page

--- src/entry-client.js
+++ src/entry-client.js
@@ -58,7 +58,7 @@ Vue.mixin({
     }).catch(({ err, res, }) => { 
       debug(err, res) 
       const domain = get(store, 'state.setting.DOMAIN')
-      removeToken(domain).then(() => next('/login')) 
+      removeToken(domain).then(() => location.replace('/login')) 
     })
   },
   beforeRouteUpdate (to, from, next) {
