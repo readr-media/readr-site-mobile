f108bdd3ab4957901d3808fd276b130f2eec6ef6
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-06-04 18:37:16 +0800

    have the series page leadging show the project heroimage and title (#189)
    * have the series page leadging show the project heroimage and title
    
    * pust navBar back

--- src/components/projects/ProjectIntroSingle.vue
+++ src/components/projects/ProjectIntroSingle.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="project-single-intro">
-    <div class="project-single-intro__progress-bar" :style="{ width: `${currProgress}%` }">
-      <div class="current-progress"><span v-text="`${currProgress}%`"></span></div>
-    </div>
-    <div class="project-single-intro__container">
+    <div class="project-single-intro__container" :style="{ backgroundImage: `url(${getImageUrl(get(project, 'heroImage'))})`, }">
+      <div class="project-single-intro__title">
+        <span v-text="title"></span>
+      </div>
       <div class="project-single-intro__desc">
         <span v-text="desc"></span>
       </div>
+    </div>
+    <div class="project-single-intro__comment">
       <AppArticleNav :articleType="'project'" :postId="project.slug" :commentCount="project.commentAmount || 0"></AppArticleNav>
+    </div>
+    <div class="project-single-intro__progress-bar" :style="{ width: `${targProgress}%` }">
+      <div class="current-progress"><span v-text="`${currProgress}%`"></span></div>
     </div>
   </div>
 </template>
 <script>
 import AppArticleNav from 'src/components/AppArticleNav.vue'
 import { get, } from 'lodash'
+import { getImageUrl, } from 'src/util/comm'
+// const debug = require('debug')('CLIENT:ProjectIntroSingle')
 
 export default {
   name: 'ProjectIntroSingle',
@@ -21,7 +28,7 @@ export default {
     AppArticleNav,
   },
   computed: {
-    currProgress () {
+    targProgress () {
       return get(this.project, 'progress', 0)
     },
     desc () {
@@ -30,12 +37,44 @@ export default {
     project () {
       return get(this.$store, 'state.publicProjectSingle', {})
     },
+    title () {
+      return get(this.project, 'title')
+    },
   },
-  methods: {},
+  data () {
+    return {
+      currProgress: 0,
+      isProgressRun: false,
+    }
+  },
+  methods: {
+    get,
+    getImageUrl,
+    runProgress () {
+      // debug('this.targProgress', this.targProgress)
+      const interval = setInterval(() => {
+        this.currProgress += Math.ceil(this.targProgress / 100)
+        // debug('this.currProgress', this.currProgress)
+        // debug('this.targProgress', this.targProgress)
+        if (this.currProgress >= this.targProgress) {
+          this.currProgress = this.targProgress
+          clearInterval(interval)
+        }
+      }, 10)
+    },
+  },
   mounted () {},
   props: {
-    projId: {
-      type: Number,
+    projSlug: {
+      type: String,
+    },
+  },
+  watch: {
+    targProgress () {
+      if (!this.isProgressRun) {
+        this.isProgressRun = true
+        this.runProgress()
+      }
     },
   },
 }
@@ -43,32 +82,56 @@ export default {
 <style lang="stylus" scoped>
 .project-single-intro
   width 100%
-  background-color #fff
-  padding 30px 30px 24px
+  // min-height 205px
   margin-bottom 10px
   position relative
+  background-color #fff
   &__container
+    background-position center left
+    background-size cover
+    background-repeat no-repeat
+    padding 50px 20px 20px
     width 100%
+    height 100%
     font-size 1.125rem
     line-height 1.5625rem
     font-weight normal
+    position relative
     > div:not(:first-child)
-      margin-top 20px
+      margin-top 10px
     > div:not(:last-child)
-      margin-bottom 20px
+      margin-bottom 10px
+    > div
+      position relative
+      z-index 1
+    &::before
+      background-image linear-gradient(45deg, rgba(255,255,255,1) 0%,rgba(255,255,255,1) 28%,rgba(255,255,255,0) 100%);
+      content ''
+      display block
+      position absolute
+      width 100%
+      height 100%
+      top 0
+      left 0
+  &__title
+    font-size 2rem
+    font-weight 600
+    line-height normal
+  &__comment
+    padding 0 20px
   &__progress-bar
     position absolute
     height 5px
     top 0
     left 0
     background-color #ddcf21
-    transition width 1s
+    transition width 1.5s
     .current-progress
       width 28px
       height 28px
       position absolute
       right -14px
-      top -5px
+      top -11px
       background-color #ddcf21
       border-radius 50%
       display flex
@@ -76,4 +139,5 @@ export default {
       align-items center
       color #fff
       font-size 0.625rem
+      z-index 999
 </style>
