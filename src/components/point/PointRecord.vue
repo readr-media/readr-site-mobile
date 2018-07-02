b91af36908ab6df0430e07daf6310c95188d0642
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-07-02 14:42:56 +0800

    implment point detail light box

--- src/components/point/PointRecord.vue
+++ src/components/point/PointRecord.vue
@@ -1,27 +1,42 @@
 <template> 
   <div class="point-record"> 
-    <template v-for="item in records"> 
-      <PointRecordItem class="point-record-item" :record="item"></PointRecordItem> 
+    <template v-for="(item, index) in records"> 
+      <PointRecordItem class="point-record-item" :record="item" @showDetail="checkoutDetail"></PointRecordItem> 
     </template> 
+    <RecordDetail :showup.sync="showDetail" :detail="currDetail"></RecordDetail>
   </div> 
 </template> 
 <script> 
   import PointRecordItem from 'src/components/point/PointRecordItem.vue' 
+  import RecordDetail from 'src/components/point/RecordDetail.vue'
   import { get, } from 'lodash' 
  
+  // const debug = require('debug')('CLIENT:PointRecord')
   const fetchPointRecords = (store) => store.dispatch('GET_POINT_HISTORIES', { params: {}, }) 
  
   export default { 
     name: 'PointRecord', 
     components: { 
       PointRecordItem, 
+      RecordDetail,
     }, 
     computed: { 
       records () { 
         return get(this.$store, 'state.pointHistories') 
       }, 
     }, 
-    methods: {}, 
+    data () {
+      return {
+        currDetail: {},
+        showDetail: false,
+      }
+    },
+    methods: {
+      checkoutDetail (detail) {
+        this.currDetail = detail
+        this.showDetail = true
+      },
+    }, 
     mounted () { 
       fetchPointRecords(this.$store) 
     }, 
