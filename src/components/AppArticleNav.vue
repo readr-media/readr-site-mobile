b884b1b272912709ca5765a6499609fe868a768d
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-10-17 16:36:56 +0800

    have project follow-clicking be alerted on unlogged-in status for project page

--- src/components/AppArticleNav.vue
+++ src/components/AppArticleNav.vue
@@ -48,7 +48,7 @@ const updateEmotion = (store, { resource = 'post', action = 'insert', emotion = 
     },
   })
 }
-const switchOn = (store, message) => store.dispatch('LOGIN_ASK_TOGGLE', { active: true, message, })
+const switchOn = (store, message) => store.dispatch('LOGIN_ASK_TOGGLE', { active: true, message, type: 'GO_LOGIN', })
 const toogleFollowingByUserStat = (store, { resource, resourceType = '', targetId, }) => {
   return store.commit('TOOGLE_FOLLOWING_BY_USER_STAT', {
     params: {
