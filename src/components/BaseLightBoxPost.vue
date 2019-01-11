cdd1c4607f9948527fecf10b755ee61a9aec699b
Noah Tan <tempo0829@gmail.com>
2019-01-11 15:07:16 +0800

    add highcharts to iframe whitelist

--- src/components/BaseLightBoxPost.vue
+++ src/components/BaseLightBoxPost.vue
@@ -125,7 +125,7 @@ export default {
       shouldRenderComment: false,
       allowedTags: [ 'img', 'strong', 'h1', 'h2', 'figcaption', 'em', 'blockquote', 'a', 'iframe', ],
       allowedAttributes: Object.assign({}, sanitizeHtml.defaults.allowedAttributes, { iframe: [ 'frameborder', 'allowfullscreen', 'src', 'width', 'height', ], }),
-      allowedIframeHostnames: [ 'www.youtube.com', 'dev.readr.tw', 'www.readr.tw', ],
+      allowedIframeHostnames: [ 'www.youtube.com', 'dev.readr.tw', 'www.readr.tw', 'cloud.highcharts.com', ],
     } 
   }, 
   methods: {
