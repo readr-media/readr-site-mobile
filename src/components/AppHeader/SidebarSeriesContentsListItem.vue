f21ae5f5c901e21aad43f292f5b881ff1c379e78
Noah Tan <tempo0829@gmail.com>
2019-07-30 12:32:58 +0800

    adjust report link (#572)

--- src/components/AppHeader/SidebarSeriesContentsListItem.vue
+++ src/components/AppHeader/SidebarSeriesContentsListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="list-item-wrapper">
-    <router-link
-      :to="url"
+    <LinkItem
+      :href="url"
       class="list-item"
       @click.native="SET_SHOW_SIDEBAR(false)"
     >
@@ -23,7 +23,7 @@
           {{ description }}
         </p>
       </div>
-    </router-link>
+    </LinkItem>
   </li>
 </template>
 
@@ -34,7 +34,12 @@ import dayjs from 'dayjs'
 
 import { createPost } from 'src/util/post'
 
+import LinkItem from 'src/components/common/LinkItem.vue'
+
 export default {
+  components: {
+    LinkItem
+  },
   props: {
     item: {
       type: Object,
